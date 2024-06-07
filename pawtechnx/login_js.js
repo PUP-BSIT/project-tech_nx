c031d40a7d4316d7ed87e188e5cad3a2b48926d2
Sofia <sabarrantes2911@gmail.com>
2024-06-07 22:41:39 +0800

    remove unecessary comments in login_js.js

--- pawtechnx/login_js.js
+++ pawtechnx/login_js.js
@@ -3,23 +3,20 @@ const imageContainer = document.querySelector('.image-container');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 
-// Function to adjust the position of the image container
 function adjustImageContainerPosition() {
     if (wrapper.classList.contains('active')) {
-        imageContainer.classList.add('register-active'); // Add the class to adjust the position when the registration form is active
+        imageContainer.classList.add('register-active'); 
     } else {
-        imageContainer.classList.remove('register-active'); // Remove the class when the login form is active
+        imageContainer.classList.remove('register-active'); 
     }
 }
 
-// Event listener for registration link click
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
-    adjustImageContainerPosition(); // Call the function to adjust the position when the registration form is activated
+    adjustImageContainerPosition(); 
 });
 
-// Event listener for login link click
 loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
-    adjustImageContainerPosition(); // Call the function to adjust the position when the login form is activated
+    adjustImageContainerPosition();
 });
