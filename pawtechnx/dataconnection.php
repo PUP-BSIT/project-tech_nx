5e9b6d22a7a5049302641fe7ebc2eabdb1f41feb
Sofia <sabarrantes2911@gmail.com>
2024-06-22 14:17:56 +0800

    update dataconnection.php

--- pawtechnx/dataconnection.php
+++ pawtechnx/dataconnection.php
@@ -9,10 +9,5 @@ $conn = mysqli_connect($servername, $username, $password,  $database);
 if (!$conn) {
  
     die("Connection failed: " . mysqli_connect_error());
-<<<<<<< HEAD
 }
-=======
- 
-} 
->>>>>>> 5615069904b3655ae591a7b4e05902dbb4d69484
 ?>
