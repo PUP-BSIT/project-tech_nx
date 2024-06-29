ff72bb145d6f0d9d72fd6b44c96834460694692d
Sofia <sabarrantes2911@gmail.com>
2024-06-29 23:03:07 +0800

    implement guard clause

--- pawtechnx/html/display_pet_details.js
+++ pawtechnx/html/display_pet_details.js
@@ -7,20 +7,21 @@ function showDetails(petID) {
             return response.json();
         })
         .then(data => {
-            if (data) {
-                document.getElementById('modal-body').innerHTML = `
-                    <h2>${data.name}</h2>
-                    <img src="${data.profile_image}" alt="Image of ${data.name}"
-                     style="width: 100%; height: auto;">
-                    <p><strong>Age:</strong> ${data.age}</p>
-                    <p><strong>Species:</strong> ${data.species}</p>
-                    <p><strong>Breed:</strong> ${data.breed}</p>
-                    <p><strong>Gender:</strong> ${data.gender}</p>
-                `;
-                document.getElementById('myModal').style.display = "block";
-            } else {
+            if (!data) {
                 console.error('No data received from server');
+                return;
             }
+
+            document.getElementById('modal-body').innerHTML = `
+                <h2>${data.name}</h2>
+                <img src="${data.profile_image}" alt="Image of ${data.name}"
+                 style="width: 100%; height: auto;">
+                <p><strong>Age:</strong> ${data.age}</p>
+                <p><strong>Species:</strong> ${data.species}</p>
+                <p><strong>Breed:</strong> ${data.breed}</p>
+                <p><strong>Gender:</strong> ${data.gender}</p>
+            `;
+            document.getElementById('myModal').style.display = "block";
         })
         .catch(error => {
             console.error('Error fetching pet details:', error);
