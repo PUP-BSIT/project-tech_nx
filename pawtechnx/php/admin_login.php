2290023e60c87a2661975878b33de34ef9234df1
Eliza Alimasa <alimasaxeliza@gmail.com>
2024-06-29 12:36:43 +0800

    add changes

--- pawtechnx/php/admin_login.php
+++ pawtechnx/php/admin_login.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <link href="../style/registration.css" rel="stylesheet" />
+    <link href="../style/login_style.css" rel="stylesheet" />
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Registration</title>
+    <title>Login</title>
   </head>
   <body>
     <div class="banner">
@@ -17,64 +17,45 @@
           <li class="dropdown">
             <button class="dropbtn">Adopt</button>
             <div class="dropdown-content">
-              <a href="../html/cats.html">Cats</a>
-              <a href="../html/hamsters.html">Hamsters</a>
-              <a href="../html/dogs.html">Dogs</a>
-              <a href="../html/rabbits.html">Rabbits</a>
+              <a href="./cats.html">Cats</a>
+              <a href="./hamsters.html">Hamsters</a>
+              <a href="./dogs.html">Dogs</a>
+              <a href="./rabbits.html">Rabbits</a>
             </div>
           </li>
-          <li><a href="../html/about_us.html">About Us</a></li>
+          <li><a href="./about_us.html">About Us</a></li>
           <div class="login">
-            <li><a href="../php/login.php">Log in</a></li>
+            <li><a href="./Login.html">Log in</a></li>
           </div>
         </ul>
       </div>
     </div>
-    <!--<div class="image-container">
+    <!-- <div class="image-container">
       <img
         src="https://s3-alpha-sig.figma.com/img/0e01/5e3a/c73b5846f7ac4b5aa450d2355cfb9e06?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dxHRCG5s4mVjVW3yvy7FBc-Vn7Lweb4pN1ZWL-J4HpjMSyzFabzzMHXNHbkNdQMXn1cuF72pCN1j~HqNTrgrePlLaXuFoXjIHdu2SF355c89L3966Np1YoYeTbHDMrD3i7W4LJFnPOKfU1vUG~90Vv59a-vAIhPiIxuPA3qJd2EAjMlrpIZ6VyaUJBJE7~AJu3sRSCo-UCHqKDCgNtVIU8ktSmnVVQTvxETNK~56i3csPRj4PJoTQ49qBKpf1XksDq~ioDPJxYfc21uFRw94Zi8WAKkEgv3PxcLJEHNBqpm7wHvV5hlnZa6iTgpnJqGjSyL6fhSAHdKPZkUVQpF-Ag__"
         alt="Decorative Image"/>
     </div>-->
     <div class="wrapper">
-      <div class="form-register">
-        <h2>Registration</h2>
-        <form action="./registration.php" method="POST">
+      <div class="form-login">
+        <h2>Login</h2>
+        <form>
           <div class="input-box">
-            <label for="fname">First Name</label>
-            <input type="text" name="fname" id="fname" required />
+            <label for="username">Admin Code</label>
+            <input type="text" id="username" name="username" required>
           </div>
           <div class="input-box">
-            <label for="lname">Last Name</label>
-            <input type="text" name="lname" id="lname" required />
+            <input type="password" name="pass" required />
+            <label>Password</label>
           </div>
-          <div class="input-box">
-            <label for="contact">Number</label>
-            <input type="tel" name="contact" id="contact" required />
+          <div class="remember-forgot">
+            <label><input type="checkbox" /> Remember me</label>
+            <a href="#">Forgot Password</a>
           </div>
-          <div class="input-box">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" required />
-          </div>
-          <div class="input-box">
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" required />
-          </div>
-          <div class="input-box">
-            <label for="confirm">Confirm Password</label>
-            <input type="password" name="confirm" id="confirm" required />
-          </div>
-          <button
-            type="submit"
-            class="btn"
-            name="submit"
-            onclick="submitUser()">
-            Register
-          </button>
+          <button type="submit" class="btn" name="login">Login</button>
           <div class="login-register">
-            <p>Already have an account? <a href="Login.html">Login</a></p>
-          </div>
-          <div class="login-register">
-            <p>Are you an admin? Proceed <a href="admin_login.html">here</a></p>
+            <p>
+              Are you a user? Proceed <a href="registration.html"> here</a>
+            </p>
           </div>
         </form>
       </div>
@@ -102,6 +83,5 @@
         <p>About Us</p>
       </div>
     </div>
-    <script src="registration.js"></script>
   </body>
 </html>
--- pawtechnx/php/register_process.php
+++ pawtechnx/php/register_process.php
@@ -6,32 +6,21 @@ $username_db = "root";
 $password_db = "";
 $dbname = "db_adoption";
 
-// Create connection
 $conn = mysqli_connect($servername, $username_db, $password_db, $dbname);
 
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Collect form data
 $username = $_POST['username'];
 $password = $_POST['password'];
 $email = $_POST['email'];
 
-// Escape variables for security
-$username = mysqli_real_escape_string($conn, $username);
-$password = mysqli_real_escape_string($conn, $password);
-$email = mysqli_real_escape_string($conn, $email);
-
-// SQL query to insert user data
 $sql = "INSERT INTO users (username, password, email, role) 
         VALUES ('$username', '$password', '$email', 'adopter')";
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
-
-    // Redirect to login page after successful registration
     header('Location: login.php');
     exit();
 } else {
