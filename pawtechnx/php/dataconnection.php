2c7116cc265fee421d3e220f51c7e2bd5a0be779
Sofia <sabarrantes2911@gmail.com>
2024-06-30 20:56:59 +0800

    update credentials

--- pawtechnx/php/dataconnection.php
+++ pawtechnx/php/dataconnection.php
@@ -1,15 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "db_adoption";
+$servername = "srv1367.hstgr.io";
+$username = "u945995174_pawtechnx";
+$password = "TECHNiX_123456789";
+$dbname = "u945995174_project";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-echo "connected!";
-
 ?>
