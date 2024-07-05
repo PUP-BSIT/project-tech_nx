e82dc116d3a03d37f7dfc3cd4f7b264f9c66c9d1
Eliza Alimasa <alimasaxeliza@gmail.com>
2024-07-06 04:40:08 +0800

    add changes

--- pawtechnx/php/login.php
+++ pawtechnx/php/login.php
@@ -13,24 +13,8 @@
   <div class="banner">
     <div class="nav-bar">
       <div class="logo">
-        <p>PAWTECHNX</p>
+        <a href="../php/landing_page.php">PAWTECHNX</a>
       </div>
-      <ul>
-        <li><a href="../php/landing_page.php">Home</a></li>
-        <li class="dropdown">
-          <button class="dropbtn">Adopt</button>
-          <div class="dropdown-content">
-            <a href="../html/cats.html">Cats</a>
-            <a href="../html/hamsters.html">Hamsters</a>
-            <a href="../html/dogs.html">Dogs</a>
-            <a href="../html/rabbits.html">Rabbits</a>
-          </div>
-        </li>
-        <li><a href="../html/about_us.html">About Us</a></li>
-        <div class="login">
-          <li><a href="../php/login.php">Log in</a></li>
-        </div>
-      </ul>
     </div>
   </div>
   <div class="wrapper">
@@ -42,8 +26,8 @@
           <input type="text" id="username" name="username" required>
         </div>
         <div class="input-box">
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password" required>
         </div>
         <div class="remember-forgot">
           <label><input type="checkbox" /> Remember me</label>
@@ -55,35 +39,10 @@
             Don't have an account? <a href="../html/registration.html"> Register</a>
           </p>
         </div>
-        <div class="login-register">
-          <p>Are you an admin? Proceed <a href="admin_login.html">here</a></p>
-        </div>
       </form>
     </div>
   </div>
-  <div class="footer">
-    <div class="logo">
-      <p>PAWTECHNX</p>
-    </div>
-    <div class="address">
-      <h4>Address</h4>
-      <p>Blk 123</p>
-      <p>Lower Bicutan</p>
-      <p>Taguig City</p>
-    </div>
-    <div class="contact">
-      <h4>Contacts</h4>
-      <p>0912345678</p>
-      <p>1234567890</p>
-      <p>email@example.com</p>
-    </div>
-    <div class="info">
-      <h4>Information</h4>
-      <p>Privacy Policy</p>
-      <p>FAQ</p>
-      <p>About Us</p>
-    </div>
-  </div>
+
 </body>
 
 </html>
