cc20045188b74cd59d26fbba927b7b6098e55575
alona pepito <pepitoalona231@gmail.com>
2024-07-06 07:42:14 +0800

    fix file database connections

--- pawtechnx/php/get_pets.php
+++ pawtechnx/php/get_pets.php
@@ -1,12 +1,6 @@
 <?php
 include "dataconnection.php";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
 $sql = "SELECT id, name, age, species, breed, gender, profile_image, 
 current_datetimestamp, updated_timestamp FROM pets";
 $result = mysqli_query($conn, $sql);
