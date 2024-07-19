efd1f1e27b0a4db766a83d92ff32bf93999193b9
Eliza Alimasa <alimasaxeliza@gmail.com>
2024-07-19 21:52:58 +0800

    add files and changes in user list page

---
+++ pawtechnx/script/user_edit.js
@@ -0,0 +1,7 @@
+function confirmUpdate() {
+    return confirm("Are you sure you want to update?");
+}
+
+function confirmDelete() {
+    return confirm("Are you sure you want to delete this user?");
+}
