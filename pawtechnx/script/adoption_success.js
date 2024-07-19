f05a21890ca61b817e97889f3bafeb781aac82c0
Sofia <sabarrantes2911@gmail.com>
2024-07-20 06:34:25 +0800

    add changes

--- pawtechnx/script/adoption_success.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const continueButton = document.getElementById('continue-button');
-    const lastVisitedPage = sessionStorage.getItem('lastVisitedPage') || 'default-pet-page.html'; // Default page if not set
-
-    continueButton.addEventListener('click', () => {
-        window.location.href = lastVisitedPage;
-    });
-});
